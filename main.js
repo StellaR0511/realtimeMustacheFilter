4d7c792befb036b7c2147d35a5c77624838931e2
StellaR0511 <81784995+StellaR0511@users.noreply.github.com>
2022-07-15 14:34:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 function setup(){
 canvas = createCanvas(300,300);
 canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+video.size(400,300);
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on("pose", gotPoses);
 }
 
 function preload(){
 }
 
 function draw(){
+image(video,0,0,400,300);
+
+}
+function modelLoaded(){
+    console.log("Model is Loaded!");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
 }
